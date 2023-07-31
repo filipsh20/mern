a5bd97ed609f7e79175f4118d7e0af0fc55f7dc7
JaumeDla <renacimiento-pruebas@outlook.es>
2023-07-31 12:08:25 +0200

    mern

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import multer from 'multer';
-import helmet from 'helmet';
-import morgan from 'morgan';
-import path from 'path';
-import { fileURLToPath } from 'url';
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import multer from "multer";
+import helmet from "helmet";
+import morgan from "morgan";
+import path from "path";
+import { fileURLToPath } from "url";
 
-import authRoutes from './routes/auth.js';
-import userRoutes from './routes/user.js';
+import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/user.js";
 
 //Configurations
 const __filename = fileURLToPath(import.meta.url);
@@ -29,23 +29,29 @@ app.use(express.static(path.resolve(__dirname, "../public")));
 
 //File storage
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) { cb(null, "public") },
-    filename: function (req, file, cb) { cb(null, file.originalname) }
-})
+  destination: function (req, file, cb) {
+    cb(null, "public");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
 const upload = multer({ storage });
 
-
-app.post('/save', upload.single("image"), (req, res) => console.log('wenas'))
+app.post("/save", upload.single("image"), (req, res) => console.log("wenas"));
 
 //routes
-app.use('/auth', authRoutes);
+app.use("/auth", authRoutes);
 app.use("/user", userRoutes);
 
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.MONGO_DB, {
+mongoose
+  .connect(process.env.MONGO_DB, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => {
-    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
-}).catch((error) => console.log(error))
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
+  })
+  .catch((error) => console.log(error));
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,12 +1,9 @@
-import express from 'express';
-import {
-    register,
-    login
-} from "../controllers/auth.js";
+import express from "express";
+import { register, login } from "../controllers/auth.js";
 
 const router = express.Router();
 
-router.post('/register', register);
-router.post('/signin', login);
+router.post("/register", register);
+router.post("/signin", login);
 
-export default router
+export default router;
